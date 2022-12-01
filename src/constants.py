ab5f67676793bb661dbf767ee9872af7b937a815
Dmytro Kasperovych <dimakasperovich1720@gmail.com>
2022-12-01 19:54:03 +0100

    source names added to .env

--- src/constants.py
+++ src/constants.py
@@ -9,5 +9,5 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URL: str = os.environ['DATABASE_URL']
 
-CATALOG_NAMES = frozenset('Subgroup Product'.split())
-RETAILER_NAMES = frozenset('Tavria Silpo Epicentr'.split())
+CATALOG_NAMES = frozenset(os.environ['CATALOG_NAMES'].split())
+RETAILER_NAMES = frozenset(os.environ['RETAILER_NAMES'].split())
