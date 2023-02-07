c6a66a6097c4d3df6af33ed9527c2c6532ae0f9b
Dmytro Kasperovych <dimakasperovich1720@gmail.com>
2023-02-07 10:37:02 +0100

    PriceLine model: 1. date_created changed 2. from_tuple method added 3. __hash__ added 4. __eq__ added 5. __repr__ added

--- src/core/models.py
+++ src/core/models.py
@@ -1,7 +1,12 @@
 """Core models."""
+from __future__ import annotations
+
 from base_models import BaseWithID
 
-from sqlalchemy import Column, ForeignKey, Numeric
+from project_typing import PriceRecord
+
+from sqlalchemy import Column, Date, ForeignKey, Numeric
+from sqlalchemy import func
 
 
 class PriceLine(BaseWithID):
@@ -14,3 +19,27 @@ class PriceLine(BaseWithID):
 
     retail_price = Column(Numeric(scale=2))
     promo_price = Column(Numeric(scale=2))
+
+    date_created = Column(Date(), server_default=func.now())
+
+    @classmethod
+    def from_tuple(cls, record: PriceRecord) -> PriceLine:
+        return cls(
+            product_id=record[0],
+            retailer_id=record[1],
+            retail_price=record[2],
+            promo_price=record[3],
+        )
+
+    def __hash__(self) -> int:
+        return hash((self.product_id, self.retailer_id,
+                     self.retail_price, self.promo_price))
+
+    def __eq__(self, __o: object) -> bool:
+        return hash(self) == hash(__o)
+
+    def __repr__(self) -> str:
+        return ('PriceLine >> prod_id: {}, ret_id: {}'
+                'retail: {}, promo: {}, created: {}.'
+                .format(self.product_id, self.retailer_id, self.retail_price,
+                        self.promo_price, self.date_created))
