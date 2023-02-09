9b1289564ce46703a80b5f57af7eb2b896329946
Dmytro Kasperovych <dimakasperovich1720@gmail.com>
2023-02-09 08:12:09 +0100

    Protocols added for FactoryCreator nad Factory

--- src/parsers/tavria/tavria_typing.py
+++ src/parsers/tavria/tavria_typing.py
@@ -1,7 +1,12 @@
 """Tavria parser typing."""
-from typing import Generator, NamedTuple
+from collections import deque
+from typing import Generator, NamedTuple, Protocol
+
+import aiohttp
 
 from catalog.models import BaseCatalogElement
+
+from retailer.models import Retailer
 
 
 class ObjectParents(NamedTuple):
@@ -17,3 +22,25 @@ BaseFactoryReturnType = Generator[BaseCatalogElement, None, None]
 Parents = tuple[str, str | None, str]
 
 NameRetailPromo = tuple[str, float, float | None]
+
+
+class Factory_P(Protocol):
+    """Factory protocol."""
+
+    _main_url: str
+
+    def __init__(self, url: str, retailer_id: int) -> None:
+        ...
+
+    async def run(self, aio_session: aiohttp.ClientSession) -> None:
+        """Run factory to add it results to box."""
+
+
+class FactoryCreator_P(Protocol):
+    """FactoryCreator protocol."""
+
+    def __init__(self, factory_cls: type[Factory_P]) -> None:
+        ...
+
+    def create(self, retailer: Retailer) -> deque[Factory_P]:
+        """Create new factories. Retailer must be specified."""
