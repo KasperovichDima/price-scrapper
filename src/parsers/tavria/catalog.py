6bd745ce61b6b4aec9f4869d349ec81c626a9a66
Dmytro Kasperovych <dimakasperovich1720@gmail.com>
2023-02-14 11:08:10 +0100

    1. path_to_id_parent_id converted to path_to_id 2. adding new folder id refactored

--- src/parsers/tavria/catalog.py
+++ src/parsers/tavria/catalog.py
@@ -1,5 +1,5 @@
 import itertools
-from collections import defaultdict, deque, namedtuple
+from collections import defaultdict, deque
 from typing import Iterable
 
 from catalog.models import Folder
@@ -14,10 +14,6 @@ from . import utils as u
 from .tavria_typing import Path
 
 
-IdParentID = namedtuple('IdParentID', ('id parent_id'))
-PathToIdParentID = dict[Path, IdParentID]
-
-
 class Catalog:
 
     _folders_in_db: list[Folder]  # TODO: Make property
@@ -27,7 +23,7 @@ class Catalog:
     _ids_to_actualize: deque[int] = deque()
     _ids_to_deprecate: set[int] = set()
     _id_to_folder: dict[int, Folder] = {}
-    _path_to_id_parent_id: PathToIdParentID = {}
+    _path_to_id: dict[Path, int] = {}
     _pathes_to_create: defaultdict[ElType, deque[Path]] = defaultdict(deque)
 
     def __init__(self, url: str, db_session: Session) -> None:
@@ -37,13 +33,15 @@ class Catalog:
     async def update(self) -> None:
         self._folders_in_db = await crud.get_folders(self._db_session)
         self._get_db_folders_data()
-        self._get_path_to_id_parent_id(self._folders_in_db)
+        self._get_path_to_id()
         del self._folders_in_db
         self._process_page_data()
         await self._create_new_folders()
         await self._switch_deprecated()
 
     def _get_db_folders_data(self) -> None:
+        """NOTE: Should not be used with new objects!"""
+
         for folder in self._folders_in_db:
             if folder.parent_id and folder.parent_id not in self._has_childs:
                 self._has_childs.add(folder.parent_id)  # type: ignore
@@ -51,10 +49,10 @@ class Catalog:
                 else self._ids_to_deprecate.add(folder.id))  # type: ignore
             self._id_to_folder[folder.id] = folder  # type: ignore
 
-    def _get_path_to_id_parent_id(self, folders: Iterable[Folder]) -> None:
-        """NOTE: Should not be used for new objects!"""
+    def _get_path_to_id(self) -> None:
+        """NOTE: Should not be used with new objects!"""
 
-        for folder in folders:
+        for folder in self._folders_in_db:
             if not folder.parent_id:  # CATEGORY
                 path = (folder.name, None, None)
             elif folder.id in self._has_childs:  # SUBCATEGORY
@@ -65,8 +63,7 @@ class Catalog:
                 path = (gp_name if gp_name else p_name,  # type: ignore
                         p_name if gp_name else None,
                         folder.name)
-            self._path_to_id_parent_id[path] = IdParentID(folder.id,  # type: ignore
-                                                          folder.parent_id)
+            self._path_to_id[path] = folder.id  # type: ignore
 
     def _get_group_parents(self, folder: Folder) -> tuple[str, str | None]:
         """Get parent name and grand parent name, if exists."""
@@ -78,9 +75,8 @@ class Catalog:
     def _process_page_data(self) -> None:
         for path in u.get_page_catalog_folders(self._url):
             try:  # folder already exists
-                path_id: int = self._path_to_id_parent_id[path].id
-                self._check_id_for_deprecation(path_id)
-            except KeyError:  # new folder
+                self._check_id_for_deprecation(self._path_to_id[path])
+            except KeyError:  # folder is new
                 self._add_path_to_create(path)
 
     def _check_id_for_deprecation(self, id_: int) -> None:
@@ -100,26 +96,20 @@ class Catalog:
 
     async def _create_new_folders(self) -> None:
         for type_, pathes in self._pathes_to_create.items():
-            if not pathes:
-                continue
             folders_to_save = []
+            path_to_folder = {}
             for path in pathes:
-                folders_to_save.append(
-                    Folder(name=self._get_folder_name(path),
-                           parent_id=self._get_parent_id(path),
-                           el_type=type_)
-                )
-
-                folders_to_save[-1].has_childs = True if not path[2] else False
+                folder = Folder(name=self._get_folder_name(path),
+                                parent_id=self._get_parent_id(path),
+                                el_type=type_)
+                folders_to_save.append(folder)
+                path_to_folder[path] = folder
 
             await crud.add_instances(folders_to_save, self._db_session)
-
-            for folder in folders_to_save:
-                if folder.has_childs:
-                    self._has_childs.add(folder.id)
-
             self._id_to_folder.update({_.id: _ for _ in folders_to_save})
-            self._get_path_to_id_parent_id(folders_to_save)
+            self._path_to_id.update({path: folder.id for path, folder  # type: ignore
+                                     in path_to_folder.items()})
+
 
     @staticmethod
     def _get_folder_name(path: Path) -> str:
@@ -135,7 +125,7 @@ class Catalog:
             if path[ind]:
                 path = path[:ind] + (None,) + path[ind + 1:]
                 try:
-                    return self._path_to_id_parent_id[path].id
+                    return self._path_to_id[path]
                 except KeyError:
                     return self._get_parent_id(path)
 
