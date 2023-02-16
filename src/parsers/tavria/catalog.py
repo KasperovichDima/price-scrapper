7f5a6cb3d979974e36e168d49e797d432073bc33
Dmytro Kasperovych <dimakasperovich1720@gmail.com>
2023-02-16 10:10:37 +0100

    big catalog refactoring

--- src/parsers/tavria/catalog.py
+++ src/parsers/tavria/catalog.py
@@ -29,8 +29,8 @@ class PathesToCreate:
             self.__subcategories.append(path)
 
     @property
-    def pathes(self) -> deque[Path]:
-        return self.__categories + self.__subcategories + self.__groups
+    def path_batches(self) -> tuple[deque[Path]]:
+        return (self.__categories, self.__subcategories, self.__groups)
 
     def clear(self) -> None:
         self.__categories.clear()
@@ -39,6 +39,9 @@ class PathesToCreate:
 
 
 class Catalog:
+
+    _url: str
+    _db_session: Session
 
     _db_folders: list[Folder]
     _ids_with_childs: set[int] = set()
@@ -51,23 +54,51 @@ class Catalog:
 
     _to_create = PathesToCreate()
 
-    def __init__(self, url: str, db_session: Session) -> None:
+    async def initialize(self, url: str, db_session: Session) -> None:
         self._url = url
         self._db_session = db_session
 
-    async def initialize(self) -> None:
-        self._db_folders = await crud.get_folders(self._db_session)
+        self._db_folders = await crud.get_folders(db_session)
 
         self._get_ids_with_childs()
 
         for folder in self._db_folders:
-            (self._deprecated_ids.add(folder.id) if folder.deprecated  # type: ignore
-                else self._ids_to_deprecate.add(folder.id))  # type: ignore
+            self._sort_deprecated(folder)
+            self._update_tables(folder)
 
+    def _get_ids_with_childs(self) -> None:
+        for folder in self._db_folders:
+            if (folder.parent_id and folder.parent_id
+                not in self._ids_with_childs):
+                self._ids_with_childs.add(folder.parent_id)
+
+    def _sort_deprecated(self, folder: Folder) -> None:
+        (self._deprecated_ids.add(folder.id) if folder.deprecated  # type: ignore
+            else self._ids_to_deprecate.add(folder.id))  # type: ignore
+
+    def _update_tables(self, folder: Folder) -> None:
             self._id_to_folder[folder.id] = folder
-            
-            path = self._get_path(folder)
-            self._path_to_id[path] = folder.id
+            self._path_to_id[self._get_path(folder)] = folder.id
+
+    def _get_path(self, folder: Folder) -> Path:
+        if folder.id not in self._ids_with_childs:  # GROUP
+            p_name, gp_name = self._get_parents(folder)
+            path = (gp_name if gp_name else p_name,  # type: ignore
+                    p_name if gp_name else None, folder.name)
+        try:  # SUBCATEGORY
+            parent = self._id_to_folder[folder.parent_id]
+            path = (parent.name, folder.name, None)
+        except KeyError:  # CATEGORY
+            path = (folder.name, None, None)
+
+        return path
+
+    def _get_parents(self, folder: Folder) -> tuple[str, str | None]:
+        """Get parent name and grand parent name, if exists."""
+
+        p_folder = self._id_to_folder[folder.parent_id]  # type: ignore
+        gp_folder = self._id_to_folder.get(p_folder.parent_id)  # type: ignore
+        return p_folder.name, gp_folder.name if gp_folder else None  # type: ignore
 
     async def update(self) -> None:
         for path in u.get_page_catalog_pathes(self._url):
@@ -78,6 +109,9 @@ class Catalog:
 
         await self._create_new_folders()
 
+        if any((self._ids_to_deprecate, self._ids_to_actualize)):
+            await crud.switch_deprecated(self._to_switch, self._db_session)
+
     def _actualize_path(self, path: Path) -> None:
         id_ = self._path_to_id[path]
         if id_ in self._ids_to_deprecate:
@@ -87,34 +121,41 @@ class Catalog:
             self._ids_to_actualize.append(id_)
 
     async def _create_new_folders(self) -> None:
-        new_folders = [Folder(name=u.get_folder_name(path),
-                              parent_id=self._get_parent_id(path))
-                       for path in self._to_create.pathes]
-        await crud.add_instances(new_folders, self._db_session)
-        self._id_to_folder.update({_.id: _ for _ in new_folders})
-        self._path_to_id.update(zip(self._to_create.pathes,
-                                    (_.id for _ in new_folders)))
+        for batch in self._to_create.path_batches:
+            new_folders = [Folder(name=u.get_folder_name(path),
+                                  parent_id=self._get_parent_id(path))
+                           for path in batch]
+            await crud.add_instances(new_folders, self._db_session)
+            self._id_to_folder.update({_.id: _ for _ in new_folders})
+            self._path_to_id.update(zip(self._to_create.path_batches,
+                                        (_.id for _ in new_folders)))
+
         self._to_create.clear()
 
+    def _get_parent_id(self, path: Path) -> int | None:
+        if not any((path[1], path[2])):
+            return None
+        try:
+            return self._path_to_id[u.cut_path(path)]
+        except KeyError:
+            return self._get_parent_id(u.cut_path(path))
+
+    @property
+    def _to_switch(self) -> Iterable[Folder]:
+        return (self._id_to_folder[id_] for id_ in
+                itertools.chain(self._ids_to_deprecate,
+                                self._ids_to_actualize))
+
+    def get_id_by_path(self, path: Path) -> int:
+        """Return id of folder, described by path."""
+        return self._path_to_id[path]
+
+
+catalog = Catalog()
 
 
 
-
-
-
-
-        # self._get_path_to_id()
-        del self._folders_in_db
-        self._process_page_data()
-        await self._create_new_folders()
-        if any((self._ids_to_deprecate, self._ids_to_actualize)):
-            await crud.switch_deprecated(self._to_switch, self._db_session)
-
-    def _get_ids_with_childs(self) -> None:
-        for folder in self._db_folders:
-            if (folder.parent_id and folder.parent_id
-                not in self._ids_with_childs):
-                self._ids_with_childs.add(folder.parent_id)
+    
 
     # def _get_db_folders_data(self) -> None:
     #     """NOTE: Should not be used with new objects!"""
@@ -147,22 +188,22 @@ class Catalog:
     #     gp_folder = self._id_to_folder.get(p_folder.parent_id)  # type: ignore
     #     return p_folder.name, gp_folder.name if gp_folder else None  # type: ignore
 
-    def _process_page_data(self) -> None:
-        for path in u.get_page_catalog_pathes(self._url):
-            try:  # folder already exists
-                self._check_id_for_deprecation(self._path_to_id[path])
-            except KeyError:  # folder is new
-                self._to_create.add(path)
+    # def _process_page_data(self) -> None:
+    #     for path in u.get_page_catalog_pathes(self._url):
+    #         try:  # folder already exists
+    #             self._check_id_for_deprecation(self._path_to_id[path])
+    #         except KeyError:  # folder is new
+    #             self._to_create.add(path)
 
-    async def _create_new_folders(self) -> None:
-        new_folders = [Folder(name=u.get_folder_name(path),
-                              parent_id=self._get_parent_id(path))
-                       for path in self._to_create.pathes]
-        await crud.add_instances(new_folders, self._db_session)
-        self._id_to_folder.update({_.id: _ for _ in new_folders})
-        self._path_to_id.update(zip(self._to_create.pathes,
-                                    (_.id for _ in new_folders)))
-        self._to_create.clear()
+    # async def _create_new_folders(self) -> None:
+    #     new_folders = [Folder(name=u.get_folder_name(path),
+    #                           parent_id=self._get_parent_id(path))
+    #                    for path in self._to_create.path_batches]
+    #     await crud.add_instances(new_folders, self._db_session)
+    #     self._id_to_folder.update({_.id: _ for _ in new_folders})
+    #     self._path_to_id.update(zip(self._to_create.path_batches,
+    #                                 (_.id for _ in new_folders)))
+    #     self._to_create.clear()
 
         # for pathes in self._pathes_to_create.items:
         #     folders_to_save = []
@@ -177,17 +218,3 @@ class Catalog:
         #     self._id_to_folder.update({_.id: _ for _ in folders_to_save})
         #     self._path_to_id.update({path: folder.id for path, folder  # type: ignore
         #                             in path_to_folder.items()})
-
-    def _get_parent_id(self, path: Path) -> int | None:
-        if not any((path[1], path[2])):
-            return None
-        try:
-            return self._path_to_id[u.cut_path(path)]
-        except KeyError:
-            return self._get_parent_id(u.cut_path(path))
-
-    @property
-    def _to_switch(self) -> Iterable[Folder]:
-        return (self._id_to_folder[id_] for id_ in
-                itertools.chain(self._ids_to_deprecate,
-                                self._ids_to_actualize))
