dc92d2c763e0aaa095971e139e9b22e5d738236b
Dmytro Kasperovych <dimakasperovich1720@gmail.com>
2023-02-17 11:18:45 +0100

    add to prev commit

--- src/base_models.py
+++ src/base_models.py
@@ -1,24 +1,8 @@
 """Base models for inheritance."""
 from database.config import Base
 
-from sqlalchemy import Column, Integer
 
-
-class BaseWithID(Base):  # type: ignore
-    """Base model class with int id as primary key + __hash__ and __eq__."""
-
-    __abstract__ = True
-
-    id = Column(Integer, primary_key=True, index=True)
-
-    def __eq__(self, __o: object) -> bool:
-        return hash(self) == hash(__o)
-
-    def __hash__(self) -> int:
-        return hash(self.id)
-
-
-class BaseWithRepr(BaseWithID):
+class BaseWithRepr(Base):
     """Base model class for classes with str 'name' field.
     Implements all BaseWithID features + __repr__."""
 
