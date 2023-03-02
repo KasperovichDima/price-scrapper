a142037ef3fb51b5d485e44ca36bba1273abdf75
Dmytro Kasperovych <dimakasperovich1720@gmail.com>
2023-03-02 12:12:55 +0100

    passing only  url to factory

--- src/parsers/tavria/factory_creator.py
+++ src/parsers/tavria/factory_creator.py
@@ -2,22 +2,22 @@
 from collections import deque
 
 from . import utils as u
-from .tavria_typing import Factory_P, Retailer_P
+from .tavria_typing import Factory_P # , Retailer_P
 
 
 class FactoryCreator:
     """Implementation of FactoryCreator protocol."""
     _factories: deque[Factory_P] = deque()
 
-    def __init__(self, retailer: Retailer_P,
+    def __init__(self, home_url: str,
                  factory_cls: type[Factory_P]) -> None:
-        self.retailer = retailer
+        self.home_url = home_url
         self._factory_cls = factory_cls
 
     def create(self) -> deque[Factory_P]:
-        for tag in u.get_group_tags(self.retailer.home_url):
+        for tag in u.get_group_tags(self.home_url):
             if url := u.get_url(tag):
-                factory = self._factory_cls(url, self.retailer.id)
+                factory = self._factory_cls(url)
                 self._factories.append(factory)
 
         self._remove_discount_page()
