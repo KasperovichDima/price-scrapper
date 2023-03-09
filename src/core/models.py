7a781e2dd4fc65bbe84bff545bd028cce105e002
Dmytro Kasperovych <dimakasperovich1720@gmail.com>
2023-03-09 15:36:19 +0100

    as_tuple added to PriceLine model, hash removed

--- src/core/models.py
+++ src/core/models.py
@@ -6,7 +6,7 @@ from decimal import Decimal
 
 from database import Base, int_fk
 
-from project_typing import PriceRecord
+from project_typing import PriceTuple
 
 from sqlalchemy import ForeignKey, Numeric
 from sqlalchemy.orm import Mapped, mapped_column  # type: ignore
@@ -30,20 +30,20 @@ class PriceLine(Base):
     )
 
     @classmethod
-    def from_tuple(cls, record: PriceRecord) -> PriceLine:
+    def from_tuple(cls, record: PriceTuple) -> PriceLine:
         return cls(
             product_id=record[0],
             retailer_id=record[1],
             retail_price=record[2],
             promo_price=record[3],
         )
-
-    def __hash__(self) -> int:
-        return hash((self.product_id, self.retailer_id,
-                     self.retail_price, self.promo_price))
+    
+    def as_tuple(self) -> tuple[int_fk, int_fk, Decimal, Decimal]:
+        return (self.product_id, self.retailer_id,
+                self.retail_price, self.promo_price)
 
     def __eq__(self, __o: object) -> bool:
-        return hash(self) == hash(__o)
+        return self.as_tuple() == __o.as_tuple()
 
     def __repr__(self) -> str:
         return ('PriceLine >> prod_id: {}, ret_id: {}, '
