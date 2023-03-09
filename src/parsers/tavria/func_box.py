dd6c863fb881859e21a1eae76659756f93f53d64
Dmytro Kasperovych <dimakasperovich1720@gmail.com>
2023-03-09 15:42:24 +0100

    func box refactored

--- src/parsers/tavria/func_box.py
+++ src/parsers/tavria/func_box.py
@@ -14,13 +14,8 @@ TODO: Datatype optymization.
 TODO: Naming optymization.
 TODO: Docstrings.
 """
-from collections import deque
 from itertools import chain
-from dataclasses import dataclass, field
-
-from project_typing import PriceRecord  # TODO: we don't need it
-
-from typing import Iterable, Iterator, NamedTuple, Sequence
+from typing import Iterator, Sequence
 
 from catalog.models import Product
 
@@ -33,141 +28,100 @@ from database import int_id
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import async_sessionmaker
 
-from .tavria_typing import Catalog_P
-from .support_classes import FactoryResults, ToSwitchStatus
-from .tavria_typing import NameRetailPromo, Path
+from .support_classes import FactoryResults, ParsedProducts
+from .tavria_typing import Catalog_P, Path
 
 
-
-class Tools:  # TODO: Name
+class BoxTools:
+    """
+    Common tools, required for results processing:
+    * retailer_id - id of current retailer.
+    * sessionmaker - sessionmaker to get database session.
+    * get_folder_id method.
+    * configurate method.
+    """
     retailer_id: int
     sessionmaker: async_sessionmaker[AsyncSession]
     __catalog: Catalog_P
 
     @classmethod
     def get_folder_id(cls, path: Path) -> int:
+        """Return id: int for specified path: Path."""
         return cls.__catalog.get_id_by_path(path)
-    
+
     @classmethod
     def configurate(cls,
                     retailer_id: int,
                     sessionmaker: async_sessionmaker[AsyncSession],
                     catalog: Catalog_P) -> None:
+        """Set BoxTools configuration."""
+
         cls.retailer_id = retailer_id
         cls.sessionmaker = sessionmaker
         cls.__catalog = catalog
 
 
-@dataclass(eq=False, order=False, frozen=True, slots=True)
-class DBParsed:
-    names: set[str] = field(default_factory=set)
-    depr_ids: set[int] = field(default_factory=set)
-    actual_ids: set[int] = field(default_factory=set)
-
-
-def get_db_parsed(products: Sequence[Product]) -> DBParsed:
-    names: set[str] = set()
-    depr: set[int] = set()
-    actual: set[int] = set()
-    for _ in products:
-        depr.add(_.id) if _.deprecated else actual.add(_.id)
-        names.add(_.name)
-    return DBParsed(names, depr, actual)
-
-
-def get_to_switch(db_parsed: DBParsed, page_ids: set[int]) -> ToSwitchStatus | None:
-    to_depr = db_parsed.actual_ids - page_ids
-    to_undepr = db_parsed.depr_ids & page_ids
-    if any((to_depr, to_undepr)):
-        return ToSwitchStatus(
-            cls_ = Product,
-            ids_to_depr=to_depr,
-            ids_to_undepr=to_undepr
-        )
-
-
-# async def switch_deprecated(db_parsed: DBParsed, page_ids: set[int]) -> None:
-#     if to_switch := get_to_switch(db_parsed, page_ids):
-#         async with Tools.sessionmaker() as session:
-#             await crud.switch_deprecated(to_switch, session)
-#             await session.flush()
-
-async def switch_deprecated(db_parsed: DBParsed, page_ids: set[int], session) -> None:
-    if to_switch := get_to_switch(db_parsed, page_ids):
-        # async with Tools.sessionmaker() as session:
-        await crud.switch_deprecated(to_switch, session)
-        await session.flush()
-
-
-def get_price_records(
-        records: deque[NameRetailPromo],
-        name_to_id: dict[str, int_id]
-) -> Iterator[PriceRecord]:
-    return zip(
-            (name_to_id[rec[0]] for rec in records),
-            (Tools.retailer_id for _ in records),
-            (rec[1] for rec in records),
-            (rec[2] for rec in records),
-            strict=True
-        )
-
-
-def get_new_price_lines(
-        last_prices: Sequence[PriceLine],
-        records: deque[NameRetailPromo],
-        name_to_id: dict[str, int_id]
-) -> list[PriceLine] | None:
-    if not (price_records := tuple(get_price_records(records, name_to_id))):
+def __get_new_prices(last_prices: Sequence[PriceLine],
+                     results: FactoryResults,
+                     name_to_id: dict[str, int_id]
+                     ) -> Iterator[PriceLine] | None:
+    if not (price_tuples := tuple(results.get_price_tuples(
+        BoxTools.retailer_id, name_to_id
+    ))):
         return None
-    existing_records = {_.as_tuple() for _ in last_prices}
-    return [PriceLine.from_tuple(_) for _ in price_records
-            if _ not in existing_records]
+    existing_price_tuples = {_.as_tuple() for _ in last_prices}
+    return (PriceLine.from_tuple(_) for _ in price_tuples
+            if _ not in existing_price_tuples)
 
 
-async def add_results(results: FactoryResults) -> None:
-    async with Tools.sessionmaker() as session:
+async def __perform_adding(results: FactoryResults,
+                           session: AsyncSession) -> None:
+    folder_id = BoxTools.get_folder_id(results.folder_path)
+    db_products = await crud.get_products(session,
+                                          folder_ids=(folder_id,))
+    parsed_products = ParsedProducts.from_products(db_products)
+    name_to_id = {_.name: _.id for _ in db_products}
+
+    del db_products
+
+    if new_prod_names := results.get_new_names(parsed_products.names):
+        new_products = [Product(name=name, parent_id=folder_id)
+                        for name in new_prod_names]
+        await crud.add_instances(new_products, session)
+        await session.flush()
+        name_to_id.update({_.name: _.id for _ in new_products})
+
+        del new_products
+
+    del folder_id, new_prod_names
+
+    if to_switch := parsed_products.get_to_switch(
+        results.get_page_product_ids(name_to_id)
+    ):
+        await crud.switch_deprecated(to_switch, session)
+
+    last_prices = await crud.get_last_prices(
+        session, (BoxTools.retailer_id,),
+        prod_ids=chain(parsed_products.depr_ids, parsed_products.actual_ids)
+    )
+    if new_prices := __get_new_prices(last_prices,
+                                      results,
+                                      name_to_id):
+        await crud.add_instances(new_prices, session)
+
+
+async def save_results(results: FactoryResults) -> None:
+    """Process factory results and save them to database."""
+    async with BoxTools.sessionmaker() as session:
         async with session.begin():
-
-            print('adding data from', results.folder_path)   #DEBUG!!!
-            folder_id = Tools.get_folder_id(results.folder_path)
-            # async with Tools.sessionmaker() as session:
-            db_products = await crud.get_products(session, folder_ids=(folder_id,))
-            db_parsed = get_db_parsed(db_products)  # TODO: Naming
-            last_prices = await crud.get_last_prices(
-                chain(db_parsed.depr_ids, db_parsed.actual_ids),
-                Tools.retailer_id,
-                session
-            )
-            name_to_id = {_.name: _.id for _ in db_products}
-            new_names: list[str] = []
-            page_ids: set[int] = set()
-
-            for record in results.records:
-                name = record[0]
-                if name not in db_parsed.names:
-                    new_names.append(name)
-                    continue
-                page_ids.add(name_to_id[name])
-
-            # print(new_names)   #DEBUG!!!
-
-            if new_names:
-                new_products = [Product(name=name, parent_id=folder_id) for name in new_names]
-                # async with Tools.sessionmaker() as session:
-                await crud.add_instances(new_products, session)
-                new_products = await crud.get_products(session, prod_names=new_names)
-                print(new_products)
-                name_to_id.update({_.name: _.id for _ in new_products})  # TODO: Needed?!!!!!!!
-                del new_products
-
-            del new_names
-            
-            await switch_deprecated(db_parsed, page_ids, session)
-
-            if new_price_lines := get_new_price_lines(last_prices, results.records, name_to_id):
-                # async with Tools.sessionmaker() as session:
-                await crud.add_instances(new_price_lines, session)
-                # await session.flush()
-            
-            print(results.folder_path, 'FINISHED!')
-            await session.commit()
+            try:
+                await __perform_adding(results, session)
+                await session.commit()
+                msg = f'Results for {results.folder_path} successfully saved!'
+            except Exception as e:
+                await session.rollback()
+                msg = (f'Saving results for {results.folder_path}'
+                       f'failed with "{e.__class__.__name__}, {e}"')
+            finally:
+                #  TODO: log msg
+                print(msg)
