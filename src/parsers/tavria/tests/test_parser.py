d564df3fd3e822a85be39fb7b0ab63ecf4b8654e
Dmytro Kasperovych <dimakasperovich1720@gmail.com>
2023-03-09 15:37:17 +0100

    test parser changed

--- src/parsers/tavria/tests/test_parser.py
+++ src/parsers/tavria/tests/test_parser.py
@@ -1,7 +1,6 @@
 """Price parser tests."""
-import asyncio
 import decimal
-from typing import Iterable
+from typing import Iterable, Sequence
 
 from core.models import PriceLine
 
@@ -13,6 +12,7 @@ from parsers import TavriaParser
 from parsers.tavria import FactoryCreator
 from parsers.tavria import catalog
 from parsers.tavria import ResultHandler
+from parsers.tavria import BoxTools
 
 import pytest
 
@@ -25,17 +25,26 @@ from . import reference as r
 from .mock_classes import ProductFactory_test
 
 
-async def fake_last_price_lines(product_ids: Iterable[int],
-                                reatiler_id: int,
-                                session: AsyncSession) -> list[PriceLine]:
+async def fake_last_price_lines(
+        session: AsyncSession,
+        retailer_ids: Iterable[int],
+        prod_ids: Iterable[int] = None,
+        folder_ids: Iterable[int] = None,
+        ) -> Sequence[PriceLine]:
     """Distinct on is not supported by SQLite, so we need simulation here."""
-    stm = select(PriceLine).filter(PriceLine.retailer_id == reatiler_id,
-                                   PriceLine.product_id.in_(product_ids))\
+    ids: list[int] = []
+    if prod_ids:
+        ids.extend(prod_ids)
+    if folder_ids:
+        ids.extend(_.id for _ in await crud.get_products(session, folder_ids=folder_ids))
+
+
+    stm = select(PriceLine).filter(PriceLine.retailer_id.in_(retailer_ids),
+                                   PriceLine.product_id.in_(ids))\
                            .order_by(PriceLine.product_id.asc())\
                            .order_by(PriceLine.date_created.asc())
     all_lines = (await session.scalars(stm)).all()
-    print([_.product_id for _ in all_lines])
-    return list({_.product_id: _ for _ in all_lines}.values())
+    return {_.product_id: _ for _ in all_lines}.values()
 
 
 class TestTavriaParser:
@@ -65,7 +74,7 @@ class TestTavriaParser:
     @pytest.mark.asyncio
     async def test_product_parser(self, fake_price_lines):
         # "Distinct on" simulation (monkey patch)
-        crud.get_last_price_lines = fake_last_price_lines
+        crud.get_last_prices = fake_last_price_lines
 
         async with TestSession() as test_session:
             retailer = await crud.get_ratailer(RetailerName.TAVRIA,
@@ -73,15 +82,16 @@ class TestTavriaParser:
         await catalog.initialize(retailer.home_url, TestSession)
         f_creator = FactoryCreator(retailer.home_url, ProductFactory_test)
         parser = TavriaParser(catalog, f_creator)
-        ResultHandler.initialize(retailer.id, catalog, TestSession)
+        BoxTools.configurate(retailer.id, TestSession, catalog)
         await parser.update_products()
 
         async with TestSession() as test_session:
             result_products = await crud.get_products(test_session)
             prod_ids = (_.id for _ in result_products)
-            db_prices = set(
-                await crud.get_last_price_lines(prod_ids, 1, test_session)
-            )
+            db_prices = set(await crud.get_last_prices(
+                test_session, (1,),
+                product_ids=prod_ids,
+            ))
 
         result_actual_product_names = set(_.name for _ in result_products
                                           if not _.deprecated)
@@ -103,4 +113,3 @@ class TestTavriaParser:
             == r.ref_deprecated_product_names
         assert len(result_prices) == len(ref_prices)
         assert result_prices == ref_prices
-
